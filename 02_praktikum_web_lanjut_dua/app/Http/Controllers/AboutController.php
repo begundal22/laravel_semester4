90ad9cb51faf72d78404f6123d71743d7f98baaa
= <=>
2022-03-03 21:52:05 +0800

    02_praktikum_web_lanjut_prak2

---
+++ 02_praktikum_web_lanjut_dua/app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return "NIM : 2041720086 <br> NAMA : Dio Aulia Ari Kurnia Sandi <br> KELAS : TI2C";
+        }
+}
--- 02_praktikum_web_lanjut_dua/app/Http/Controllers/PageController.php
+++ 02_praktikum_web_lanjut_dua/app/Http/Controllers/PageController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
         public function index(){
-        return "Selamat Datang Dio";
+        return "Selamat Datang";
         }
     
         public function about(){
